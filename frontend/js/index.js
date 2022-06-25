ea59d7bcb81f7f2f864a7d7a5102016c7a244c8a
Megumi Akama <kamayatu91@gmail.com>
2022-06-24 17:03:33 -0700

    Add class in html and event in JS file

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,27 @@
-$(() => {
-  //Start coding here!
-});
+$(document).ready(() => {
+ 
+  $("#btn__add-account").on("click", (e)=> {
+    e.preventDefault();
+    let val = $("#input__account").val();
+    console.log(val);
+  });
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //End of document get ready event
