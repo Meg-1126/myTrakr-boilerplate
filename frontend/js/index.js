e79b944032b64b6ea76b23be070e7b3990e11ab3
Megumi Akama <kamayatu91@gmail.com>
2022-06-28 17:27:22 -0700

    Add validation

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,22 @@
 $(document).ready(() => {
- 
+  let accountName = [];
+
+  
   $("#btn__add-account").on("click", (e)=> {
     e.preventDefault();
-    let val = $("#input__account").val();
-    console.log(val);
-  });
+    //Validation for empty input & existing account
+    if ($.inArray($("#input__account").val(), accountName) === -1) {
+      if ($("#input__account").val().length === 0 ) {
+        alert("You must enter an account name!");
+     } else {
+       accountName.push($("#input__account").val());
+     }
+    } else {
+      alert("Account name already exists");
+    }
+      console.log(accountName);
+    
+  }); //End of click event for add new account button
 
   
   
